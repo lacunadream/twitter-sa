3ce36c0ce430eeb051788c9fda7e33b108d57f29
clouddream <d.s.yong@lse.ac.uk>
2015-08-31 01:09:55 +0100

    Added integration at node console level

--- app.js
+++ app.js
@@ -7,24 +7,30 @@ var express = require('express')
   , port = process.env.PORT || 8080
   , fs = require('fs');
 
+
   server.listen(port);
-  console.log('app running at port' + port)
+  console.log('app running at port !' + port)
+
+//Create AlchemyAPI object
+var AlchemyAPI = require('./alchemyapi');
+var alchemyapi = new AlchemyAPI();
+var output = {};
 
 //routing
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 })
 
 //Keys
-var keyarray = fs.readFileSync(__dirname + '/api_key.txt').toString().split(',')
+var keyarray = fs.readFileSync(__dirname + '/twitter_key.txt').toString().split(',')
 var consumerKey = keyarray[0];
 var consumerSecret = keyarray[1];
 var accessToken = keyarray[2];
 var accessTokenSecret = keyarray[3];
 
-//twitter initiate
+//Twitter initiate
 var api = new Twit({
   consumer_key: consumerKey,
   consumer_secret: consumerSecret,
@@ -34,13 +40,19 @@ var api = new Twit({
 
 
 //stream phrase
-var keyword = ['bersih'];
+var keyword = ['kill me'];
 
 io.sockets.on('connection', function (socket) {
-	var stream = api.stream('statuses/filter', {track:keyword})
+  var stream = api.stream('statuses/filter', {track:keyword})
 
-	stream.on('tweet', function(tweet) {
-		console.log(tweet);
-		io.sockets.emit('stream',tweet);
-	});
+  stream.on('tweet', function(tweet) {
+    console.log(tweet.text);
+    alchemyapi.sentiment('html', tweet.text, {}, function(response) {
+      output['sentiment'] = response['docSentiment']
+      console.log(output['sentiment'].type)
+      console.log(output['sentiment'].score)
+    });
+    io.sockets.emit('stream',tweet);
+
+  });
 });
